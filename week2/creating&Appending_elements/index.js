0c78e099a5684a3c14966f55177a65d3f82b4f84
haleyj13 <haley.jones@bryanuniversity.edu>
2020-10-03 10:47:07 -0700

    creating&Appending_elements

---
+++ week2/creating&Appending_elements/index.js
@@ -0,0 +1,22 @@
+var newLi = document.createElement("li")
+newLi.textContent = "This is my ordered list"
+var myList = document.getElementById("my-list")
+myList.append(newLi)
+var newLi = document.createElement("li")
+newLi.textContent = "Appending Elements"
+var myList = document.getElementById("my-list")
+myList.append(newLi)
+var newLi = document.createElement("li")
+newLi.textContent = "Hello World"
+var myList = document.getElementById("my-list")
+myList.append(newLi)
+
+var newH1 = document.createElement("h1")
+newH1.textContent = "Welcome to my JS site"
+document.body.append(newH1)
+
+var newP = document.createElement("p")
+newP.textContent = "All of this was created with Javascript"
+document.body.append(newP)
+
+
