2ae4581a269321f9ad50008abb4a470c0498447c
haleyj13 <haley.jones@bryanuniversity.edu>
2020-10-10 17:27:06 -0700

    creating_a_list

--- week3/changing_styles_wJS/index.js
+++ week3/changing_styles_wJS/index.js
@@ -20,22 +20,36 @@
 //  li5.innerHTML = "<h2> North Carolina </h2>"
 //  ul.append(li5)
 
-var states = ["California", "New York", "Texas", "Florida", "North Carolina"];
-var text = "";
-var i;
-for (i = 0; i < states.length; i++) {
-  text += states[i] + "<br>";
-}
-document.getElementById("hello").innerHTML = text;
+// var states = ["California", "New York", "Texas", "Florida", "North Carolina"];
+// var text = "";
+// var i;
+// for (i = 0; i < states.length; i++) {
+//   text += states[i] + "<br>";
+// }
+// var h2 = document.createElement("h2")
+// h2.textContent = ("Whatever text I want")
+// document.body.append(h2)
+// for(var i = 0; i < 4; i++){
+//     var h2 = document.createElement("h2")
+//     h2.textContent = ("Whatever text I want")
+//     document.body.append(h2)
+//     console.log(i)
+// }
+//  var ul = document.createElement("ul")
+//  document.body.append(ul)
+//  var li = document.createElement("li")
+//  li.textContent = [0]
+//   ul.append(li)
+// document.getElementById("h2").innerHTML = text;
 
-document.getElementById("hello").style.color = "cornflowerblue";
-document.getElementById("hello").style.fontFamily = "sans-serif";
-document.getElementById("hello").style.fontSize = "20px";
-document.getElementById("hello").style.fontWeight = "lighter";
+// document.getElementById("h2").style.color = "cornflowerblue";
+// document.getElementById("h2").style.fontFamily = "sans-serif";
+// document.getElementById("h2").style.fontSize = "20px";
+// document.getElementById("h2").style.fontWeight = "lighter";
 
-//add new-class
-document.querySelector("#hello").classList.add(".border")
-console.log(document.querySelector("#hello").className)
+// //add new-class
+// heading.classList.add(".border")
+// console.log(document.querySelector("#h2").className)
 // for (var i = 0; i < 5; i++){
 //     var h2 = document.createElement("h2");
 //      h1.textContent = ("Hello World");
@@ -43,7 +57,22 @@ console.log(document.querySelector("#hello").className)
 //      console.log(i)
 //      console.log(name[i])
 //    }
+var states = ["California", "New York", "Texas", "Florida", "North Carolina"];
+var div = document.createElement("div")
+document.body.append(div)
+
+for (i = 0; i < states.length; i++) {var heading = document.createElement("h1")
+div.append(heading)
+heading.textContent = states[i]
 
 
+heading.style.color = "cornflowerblue";
+heading.style.fontFamily = "sans-serif";
+heading.style.fontSize = "20px";
+heading.style.fontWeight = "lighter";
 
-document.getElementById("h2")
+//add new-class
+heading.classList.add("border")
+console.log(heading.className)
+
+}
--- week3/creating_a_list/index.js
+++ week3/creating_a_list/index.js
@@ -1,32 +1,33 @@
 
 
-//  var h1 = document.createElement("h1");
-//  h1.textContent = ("Hello World");
-//  document.body.append(h1)
+ var h1 = document.createElement("h1");
+ h1.textContent = ("Hello World");
+ document.body.append(h1)
 
-// var firstName = ["Steve" ,"Larry" , "Joe" , "Shirley" , "Steph" , "Nate" , "Rick" , "Emily"
-//     ];
-
+var firstName = [`Steve` ,`Larry` , `Joe` , `Shirley` , `Steph` , `Nate` , `Rick` , `Emily`
+    ];
+//     var firstName = [0, 1]
+// console.log(firstName)
 //  var ul = document.createElement("ul")
 //  document.body.append(ul)
 //  var li = document.createElement("li")
-//  li.textContent = name[0]
+//  li.textContent = firstName[0]
 //   ul.append(li)
 
 
-//  for (var i = 0; i < 10; i++){
-//   var h1 = document.createElement("h1");
-//    h1.textContent = ("Hello World");
-//    document.body.append(h1)
-//    console.log(i)
-//    console.log(name[i])
-//  }
-// console.log(typeof name)
+ for (var i = 0; i < 10; i++){
+  var h1 = document.createElement("h1");
+   h1.textContent = ("Hello World");
+   document.body.append(h1)
+   console.log(i)
+   console.log(firstName[i])
+ }
+console.log(typeof firstName)
 
- for (var index = 0; index < 8; index++) {
+ for (index = 0; index < 8; index++) {
    console.log(index)
-  console.log(name)
+  console.log(firstName)
   var flower = document.createElement("h1");
-  flower.textContent = name[index]
+  flower.textContent = firstName[index]
   document.body.append(flower)
  }
